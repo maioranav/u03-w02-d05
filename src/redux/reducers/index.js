11ffe7cbcb266e780fc0f9fbceadb0f912336ed2
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-17 10:55:57 +0100

    created redux method for favcities

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 const initialState = {
    //IL NOSTRO STATO INIZIALE
+   favs: [],
    city: { name: "", lat: 0, lon: 0 }
 }
 
@@ -10,7 +11,9 @@ const mainReducer = (state = initialState, action) => {
 
    switch (action.type) {
       case "CHANGE_CITY":
-         return state.city = { name: action.payload.city, lat: action.payload.lat, lon: action.payload.lon }
+         return state = { ...state, city: { name: action.payload.name, lat: action.payload.lat, lon: action.payload.lon } }
+      case "ADD_CITY":
+         return state = { ...state, favs: [...state.favs, action.payload.name] }
       default:
          return state;
    }
